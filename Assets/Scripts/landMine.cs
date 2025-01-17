d035371702a62d058afa2dc4bef5a83a41885b51
mjhenault <mhenault70@gmail.com>
2025-01-17 15:36:34 -0500

    Added land mines.
    Added a script for land mines calling to the game manager for the damage script to be use. Also made a prefab with a simple mine texture found for free.

---
+++ Assets/Scripts/landMine.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class landMine : MonoBehaviour
+{
+
+    [SerializeField] int damage;
+
+    bool steppedOn;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        steppedOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            steppedOn = true;
+            GameManager.instance.playerScript.takeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}

3ca67fd4360f1d314d68a223c51260a07191f7f1
mjhenault <mhenault70@gmail.com>
2025-01-20 14:02:25 -0500

    Added cubes above the enemies to identify the types.

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameTimer : MonoBehaviour
+{
+    public float totalTime = 300f;
+    public bool timerIsRunning = false;
+    private float timeRemaining;
+    public TMP_Text timerText;
+   /* public Text timerText;*/ //reference to UI text 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //to start timer
+        timeRemaining = totalTime;
+        timerIsRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {//to decrease time
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);//to update UI text
+            }
+            else
+            {
+                Debug.Log("Time's up!!!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+                TimerEnded();
+            }
+        } 
+    }
+    
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1; //timer using minutes and seconds
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);//to update the UI text
+    }
+
+    void TimerEnded()
+    {
+        Debug.Log("Timer has ended! GAME OVER");
+    }
+}

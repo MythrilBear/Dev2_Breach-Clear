4cce24fb2b5f3c3e984916a435ef4257ee7412c6
Jarreth Parkhurst <Jarreth.Parkhurst@gmail.com>
2025-01-30 03:30:07 -0600

    Updated prefabs.

--- Assets/Scripts/landMine.cs
+++ Assets/Scripts/landMine.cs
@@ -5,6 +5,7 @@ public class landMine : MonoBehaviour
 
     [SerializeField] int damage;
 
+
     bool steppedOn;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created

c2ba326547a8a3b0d4b3201baf96afb6c36f8b9f
Jarreth Parkhurst <Jarreth.Parkhurst@gmail.com>
2025-02-23 03:45:42 -0600

    Knife Functionality
    The knife can now damage enemies.

--- Assets/Scripts/Weapons/Knife.cs
+++ Assets/Scripts/Weapons/Knife.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 
-public class Knife : MonoBehaviour
+public class Knife : Gun
 {
+    public AudioSource audSource;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public override void Shoot()
     {
-        
+        RaycastHit hit;
+
+        if (Physics.Raycast(cameraTransform.position, cameraTransform.forward,
+            out hit, gunStats.shootingRange, gunStats.targetLayerMask))
+        {
+            Debug.Log(gunStats.gunName + " hit " + hit.collider.name);
+            Instantiate(gunStats.hitEffect, hit.point, Quaternion.identity);
+
+            IDamage dmg = hit.collider.GetComponent<IDamage>();
+
+            if (dmg != null)
+            {
+                dmg.takeDamage(gunStats.shootDamage);
+            }
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
+        base.Update();
+
+        if (Input.GetButtonDown("Shoot"))
+        {
+            Shoot();
+            audSource.PlayOneShot(gunStats.shootSound[Random.Range(0, gunStats.shootSound.Length)], gunStats.shootSoundVol);
+        }
+
         
     }
 }
